0f183dc1b7957de18428497b32b8ba1a1dc3782b
Vincent Xu <m197116@usna.edu>
2019-03-06 13:39:31 -0500

    Defined paths to library functions and .csv data files

--- etc/config.inc.php
+++ etc/config.inc.php
@@ -1,11 +1,18 @@
 <?php
 
   // configuration
+  define("LIB_PATH", "../lib/");
   define("WEB_PATH", "../web/");
+  define("DATA_PATH", "../data/");
   define("SCRIPTS_PATH", "../scripts/");
   define("HOME_PAGE", "/~m197116/capstone/");
 
   // include page class for HTML generation
   require_once(WEB_PATH . "page.inc.php");
+
+  // include useful library functions
+  require_once(LIB_PATH . "lib_read_csv.php");
+  require_once(LIB_PATH . "lib_tables.php");
+  require_once(LIB_PATH . "lib_functions.php");
 
 ?>
